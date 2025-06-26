9af5e5742b12cd26af8e750ca6590010822cfe67
tahaamin2013 <tahaamindob2013@gmail.com>
2025-06-26 19:28:54 +0500

    fd

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -23,7 +23,7 @@ export default function DevicesPage() {
         <div className="flex flex-col items-center">
           {/* <p>Latitude: {location.lat}</p>
           <p>Longitude: {location.lng}</p> */}
-          <div className="mt-4 w-[700px] md:w-[1000px] h-[250px]">
+          <div className="mt-4 w-[700px] md:w-[1000px] h-[500px]">
             <iframe
               width="100%"
               height="100%"
